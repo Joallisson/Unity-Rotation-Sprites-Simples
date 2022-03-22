38ff974fa1757bfc2c3164c71833a594518621e9
Joallisson Nascimento Castro <joallissons@gmail.com>
2022-03-21 22:56:22 -0300

    Criando animação do sprite

--- Rotação de Sprites Mobile/Assets/Scripts/Rotation.cs
+++ Rotação de Sprites Mobile/Assets/Scripts/Rotation.cs
@@ -6,6 +6,6 @@ public class Rotation : MonoBehaviour
 {
 
     public void girarSprite(){
-        transform.Rotate(0, 0, 45); //Girando os sprites em 45º graus
+        transform.Rotate(0, 0, 90); //Girando os sprites em 45º graus
     }
 }
